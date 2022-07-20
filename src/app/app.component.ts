64ed6390554a70a46c7502e3a026234d1c71bfe4
Manuel Greulich <manuelgreulich@ahoo.com>
2022-07-20 21:14:45 +0200

    new changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent{
   title= 'manuelgreulich';
 
+  constructor(public router: Router) {
 
-
+  }
 }
