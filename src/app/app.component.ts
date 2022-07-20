33c5f6000450ae8623c4f9be1cfa5b3c7fa3a2fc
Manuel Greulich <manuelgreulich@ahoo.com>
2022-07-20 13:04:28 +0200

    image-changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'manuelgreulich';
+export class AppComponent implements OnInit {
+  images = ['code.jpg', 'macbook.jpg', 'turm.jpg']; 
+  currentImage = 0;
+
+  ngOnInit(){
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+    }, 8000);
+  }
+
+
+
 }
