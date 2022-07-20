adafbbd5df9bea2f1347d8638478e1da08cc663c
Manuel Greulich <manuelgreulich@ahoo.com>
2022-07-20 16:24:55 +0200

    start headermenu

---
+++ src/app/slideshow/slideshow.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slideshow',
+  templateUrl: './slideshow.component.html',
+  styleUrls: ['./slideshow.component.scss']
+})
+export class SlideshowComponent implements OnInit {
+  
+  images = ['code.jpg', 'macbook.jpg', 'turm.jpg']; 
+  headlines = [
+  'Bring engineering to the next Level',
+  'Born to Code',
+  'Graduated at TUM'
+  ];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit(){
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
+    }, 8000);
+  }
+
+}
